7825ad65f716681b071ddb75ec2da42468f8c2ad
romiogaku <romiogaku@me.com>
2018-02-20 02:05:42 +0900

    add tooltip

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,11 +1,26 @@
 import * as React from "react";
+import ReactTooltip from "react-tooltip";
 import "./Grid.css";
 
 const Grid = props => {
   const listItems = props.list.map(item => {
     return (
       <li key={item.id}>
-        <img src={item.url} className="cover" alt={item.word} />
+        <img
+          src={item.url}
+          className="cover"
+          alt={item.word}
+          data-tip
+          data-for={String(item.id)}
+        />
+        <ReactTooltip
+          id={String(item.id)}
+          place="top"
+          type="dark"
+          effect="float"
+        >
+          <span>{item.word}</span>
+        </ReactTooltip>
       </li>
     );
   });
